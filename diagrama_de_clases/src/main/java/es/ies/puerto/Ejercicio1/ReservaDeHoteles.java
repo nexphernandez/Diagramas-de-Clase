3dcb34a7d1364b1e9a2214dbb08d542adce52d42
nexphernandez <nicolaseh3b1112@gmail.com>
2024-12-06 10:38:26 +0000

    Se genera los paquetes

---
+++ diagrama_de_clases/src/main/java/es/ies/puerto/Ejercicio1/ReservaDeHoteles.java
@@ -0,0 +1,7 @@
+package es.ies.puerto.Ejercicio1;
+
+public class ReservaDeHoteles {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ diagrama_de_clases/src/main/java/es/ies/puerto/Ejercicio2/SistemaBibliotecaA.java
@@ -0,0 +1,5 @@
+package es.ies.puerto.Ejercicio2;
+
+public class SistemaBibliotecaA {
+    
+}
