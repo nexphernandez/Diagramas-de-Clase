cce18fed03ef87154e569b2578e1209573388efd
nexphernandez <nicolaseh3b1112@gmail.com>
2024-12-06 14:20:20 +0000

    Se hace la clase miembro

--- diagrama_de_clases/src/main/java/es/ies/puerto/Biblioteca/Miembro.java
+++ diagrama_de_clases/src/main/java/es/ies/puerto/Biblioteca/Miembro.java
@@ -1,5 +1,88 @@
 package es.ies.puerto.Biblioteca;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class Miembro {
+    private String nombre;
+    private String id;
+    private List<Prestamo> historial;
+
+    public Miembro() {
+    }
+
+    public Miembro(String nombre, String id) {
+        this.nombre = nombre;
+        this.id = id;
+        this.historial = new ArrayList<>();
+    }
+
+    public String getNombre() {
+        return this.nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getId() {
+        return this.id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public Miembro nombre(String nombre) {
+        setNombre(nombre);
+        return this;
+    }
+
+    public Miembro id(String id) {
+        setId(id);
+        return this;
+    }
+
+    /**
+     * Metodo para registrar miembros
+     * @param miembro a registrar
+     */
+    public void registrarMiembros(Prestamo miembro){
+        historial.add(miembro);
+    }
+
+    /**
+     * Metodo para ver el historial
+     */
+    public void verHistorial(){
+        for (Prestamo prestamo : historial) {
+            System.out.println(prestamo);
+        }
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == this)
+            return true;
+        if (!(o instanceof Miembro)) {
+            return false;
+        }
+        Miembro miembro = (Miembro) o;
+        return Objects.equals(nombre, miembro.nombre) && Objects.equals(id, miembro.id) && Objects.equals(historial, miembro.historial);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(nombre, id, historial);
+    }
+
+    @Override
+    public String toString() {
+        return "{" +
+            " nombre='" + getNombre() + "'" +
+            ", id='" + getId() + "'" +
+            "}";
+    }
     
 }
